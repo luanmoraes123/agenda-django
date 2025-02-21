f5b85c5e43f7fce4131803510c5c41f8a3f42d76
Luan Moraes Alencar <luan.moraees21@gmail.com>
2025-02-21 08:02:55 -0300

    alterando urls

--- contact/admin.py
+++ contact/admin.py
@@ -4,6 +4,6 @@ from contact.models import Contact
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'id', 'first_name', 'last_name', 'phone'
+    list_display = 'id', 'first_name', 'last_name', 'phone', 'picture'
 
     search_fields = 'id', 'first_name', 'email'
