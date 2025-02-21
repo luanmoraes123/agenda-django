1cbf23b7cc49dbb876c410b6869adcfc2377eb50
Luan Moraes Alencar <luan.moraees21@gmail.com>
2025-02-21 07:54:06 -0300

    modificando model de contacts

--- contact/admin.py
+++ contact/admin.py
@@ -5,3 +5,5 @@ from contact.models import Contact
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
     list_display = 'id', 'first_name', 'last_name', 'phone'
+
+    search_fields = 'id', 'first_name', 'email'
