a2b7b580109d6154b1e6fe6495591086d5dd4347
Luan Moraes Alencar <luan.moraees21@gmail.com>
2025-02-22 10:27:23 -0300

    criando a tabela de contacts

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from contact.models import Contact
 
 
 def index(request):
-    return render(request, 'contact/index.html')
+    contacts = Contact.objects.all()
+
+    context = {
+        'contacts': contacts,
+    }
+    return render(request, 'contact/index.html', context)
