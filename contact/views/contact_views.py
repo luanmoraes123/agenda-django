568ba6d4de02e6c34d5ef93295a6ee7f7fd66467
Luan Moraes Alencar <luan.moraees21@gmail.com>
2025-06-18 10:27:05 -0300

    adicionando funcionalidade a barra de pesquisa

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
 from contact.models import Contact
 
 
 def index(request):
-    contacts = Contact.objects.filter(show=True)
+    contacts = Contact.objects.filter(show=True).order_by('-id')[10:20]
 
     context = {
         'contacts': contacts,
@@ -18,3 +19,24 @@ def contact(request, contact_id):
         'contact': single_contact,
     }
     return render(request, 'contact/contact.html', context)
+
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == "":
+        return redirect('contact:index')
+
+    contacts = Contact.objects\
+        .filter(show=True)\
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(email__icontains=search_value) |
+            Q(phone__icontains=search_value)
+        )
+
+    context = {
+        'contacts': contacts,
+    }
+    return render(request, 'contact/index.html', context)
