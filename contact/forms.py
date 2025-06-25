89d68008b918c3098120bb254704e37be019f95f
Luan Moraes Alencar <luan.moraees21@gmail.com>
2025-06-25 12:01:03 -0300

    validando e salvnado formulario

--- contact/forms.py
+++ contact/forms.py
@@ -1,5 +1,6 @@
 from .models import Contact
 from django import forms
+from django.core.exceptions import ValidationError
 
 
 class ContactForm(forms.ModelForm):
@@ -15,10 +16,6 @@ class ContactForm(forms.ModelForm):
         label="Telefone",
         help_text="Escreva o telefone aqui."
     )
-    description = forms.CharField(
-        label="Descrição",
-        help_text="Escreva a descrição aqui."
-    )
     email = forms.CharField(
         label="Email",
         help_text="Escreva o email aqui."
@@ -31,5 +28,13 @@ class ContactForm(forms.ModelForm):
             'last_name',
             'phone',
             'email',
-            'description'
+            'description',
+            'category'
         )
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+        if first_name == 'ABC':
+            self.add_error('first_name',
+                           ValidationError("Não use ABC nesse campo", code='invalid'))
+        return first_name
